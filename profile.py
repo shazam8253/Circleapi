3fd0c7042829f94e5906473e4b9036efdee6c813
Curee Devs <cureein@gmail.com>
2021-08-22 17:04:58 -0500

    commit

--- profile.py
+++ profile.py
@@ -1,7 +1,7 @@
 import profile
 from flask import Blueprint, request, Response, jsonify
 from werkzeug.utils import secure_filename
-from utils import db_write, profpic
+from utils import db_write, getallzeposts, postuno, profpic
 
 
 profile = Blueprint("profile", __name__)
@@ -13,10 +13,38 @@ def uploadprofpic():
     profilepic = request.files['image']
     if profilepic:
         print("image sent")
-        profpic( request.headers.get('Authorization'), profilepic)
+        try: 
+            profpic( request.headers.get('Authorization'), profilepic)
+            return Response(status = 200)
+        except:
+            return Response(status = 250)
         # "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTJ9.V0KLsRHxxfFpDZwnwrCwrgYWJjLNTWJeX865Z8Ns6wE"
-        return Response(status=200)
     else:
         return Response(status=250)
+
+@profile.route("/post", methods=["POST"])
+def post():
+    if 'image' not in request.files:
+        print('No file part')
+        return Response(status=250)
+    post = request.files['image']
+    if post:
+        print("image sent")
+        try: 
+            postuno(request.headers.get('Authorization'), post)
+            return Response(status = 200)
+        except:
+            return Response(status = 250)
+        # "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTJ9.V0KLsRHxxfFpDZwnwrCwrgYWJjLNTWJeX865Z8Ns6wE"
+    else:
+        return Response(status=250)
+
+@profile.route("/getallposts", methods=['GET'])
+def getallposts():
+    try:
+        getallzeposts(request.headers.get('Authorization'))
+        return Response(status = 200)
+    except:
+        return Response(status = 250)
 
 
